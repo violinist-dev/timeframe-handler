850e340aab823c8a7b8d281e48979479ace715d4
Eirik Stanghelle Morland <eirik@morland.no>
2022-08-07 08:12:51 +0200

    Update Handler.php

--- src/Handler.php
+++ src/Handler.php
@@ -2,7 +2,16 @@
 
 namespace Violinist\TimeFrameHandler;
 
+use Violinist\Config\Config;
+
 class Handler
 {
 
+    public static function createFromConfig(Config $config) 
+    {
+        $frame = $config->getTimeFrameDisallowed();
+        if (empty($frame)) {
+            return;
+        }
+    }
 }
